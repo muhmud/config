effb6e6f632aaa1ee258d32e1e2dd53cf8f69399
Muhmud Ahmad <muhmud.ahmad@googlemail.com>
2017-05-06 01:52:52 +0800

    Fixes for pg-sql handling, mappings

--- emacs/.emacs.d/my/sql/my-pg-sql.el
+++ emacs/.emacs.d/my/sql/my-pg-sql.el
@@ -24,6 +24,27 @@
   (append-sql-to-buffer)
   (append-string-to-file " > /ram/script.sql" "/ram/buffer.sql"))
 
+(defun write-pg-sql-result-to-buffer()
+  (interactive)
+  "Write the result of running some sql to a buffer file"
+  (write-string-to-file "copy (" "/ram/buffer.sql")
+  (append-sql-to-buffer)
+  (append-string-to-file ") to '/ram/script.sql' with (format csv);" "/ram/buffer.sql"))
+
+(defun write-pg-drop-table()
+  (interactive)
+  "Drop a table"
+  (write-string-to-file "drop table " "/ram/buffer.sql")
+  (append-sql-to-buffer)
+  (append-string-to-file ";" "/ram/buffer.sql"))
+
+(defun write-pg-drop-table-cascade()
+  (interactive)
+  "Drop a table"
+  (write-string-to-file "drop table " "/ram/buffer.sql")
+  (append-sql-to-buffer)
+  (append-string-to-file " cascade;" "/ram/buffer.sql"))
+
 (eval-after-load "sql"
   '(progn
      (sql-set-product 'postgres)
@@ -31,7 +52,10 @@
      (define-key sql-mode-map "\eO1;3P" 'write-pg-desc-sql-region-to-buffer) ;Alt + F1
      (define-key sql-mode-map "\eO1;3Q" 'write-pg-script-sql-region-to-buffer) ;Alt + F2
      (define-key sql-mode-map "\eO1;3R" 'insert-script) ;Alt + F3
-     (define-key sql-mode-map (kbd "M-<f5>") 'write-pg-select-sql-region-to-buffer) ;Alt + F5
+     (define-key sql-mode-map (kbd "M-<f5>") 'write-pg-sql-result-to-buffer) ;Alt + F5
+     (define-key sql-mode-map (kbd "M-<f6>") 'write-pg-select-sql-region-to-buffer) ;Alt + F6
+     (define-key sql-mode-map "\e[24;9{" 'write-pg-drop-table) ;Alt + D
+     (define-key sql-mode-map "\e[24;9}" 'write-pg-drop-table-cascade) ;Alt + Shift + D
      (define-key sql-mode-map "\e " 'write-pg-select-tables-sql-region-to-buffer) ;Alt + Space
      (define-key sql-mode-map "\e[24;9~" 'create-sql-region-and-write-to-buffer) ;Ctrl + Return
    )
